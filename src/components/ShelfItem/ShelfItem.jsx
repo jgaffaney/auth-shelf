04739c80d85a5d4b9b489bbac9f1239a7d6f2d19
SJCMN <sam.clavette@gmail.com>
2021-11-10 15:05:15 -0600

    add mui

--- src/components/ShelfItem/ShelfItem.jsx
+++ src/components/ShelfItem/ShelfItem.jsx
@@ -1,13 +1,29 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
 function ShelfItem({item}) {
 
     console.log(item.description);
     return (
-        <div>
-            
+        <Card sx={{ maxWidth: 345, m:1, p:2 }}>
+
+        <CardMedia
+          height="250"
+          
+        />
+        <CardContent>
             <img src={item.image_url} alt={item.description} />
-            <p>{item.description}</p>
-        </div>
+          <Typography variant="body2" color="text.secondary">
+          {item.description}
+          </Typography>
+        </CardContent>
+      
+      </Card>
 
     );
 };
 export default ShelfItem;
+
+
