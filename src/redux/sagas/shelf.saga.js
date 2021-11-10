ecaf0e5369ef48a94cfb234017f2e9d3e2c375f7
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-11-10 13:39:32 -0600

    shlef sagas complete, not tested

---
+++ src/redux/sagas/shelf.saga.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* fetchShelf() {
+    try{
+        const response = yield axios.get('/api/shelf');
+        yield put({type: 'SET_SHELF', payload: response.data});
+    } catch (err) {
+        console.log('Error on fetchShelf: ', err);
+        yield put({type: 'FETCH_ERROR'})
+    }
+}
+
+function* addItem(action) {
+    try{
+        yield axios.post('/api/shelf', action.payload)
+        yield put({type: 'FETCH_SHELF'})
+    } catch (err) {
+        console.log('Error on addItem: ', err);
+        yield put({type: 'ADD_ERROR'})
+    }
+}
+
+function* deleteItem(action) {
+    try{ 
+        yield axios.delete(`/api/shelf/${action.payload}`);
+        yield put({type: 'FETCH_SHELF'})
+    } catch (err) {
+        console.log('Error on delete: ', err);
+        yield put({type: 'DELETE_ERROR'})
+    }
+}
+
+function* shelfSaga () {
+    yield takeLatest('FETCH_SHELF', fetchShelf);
+    yield takeLatest('ADD_ITEM', addItem);
+    yield takeLatest('DELETE_ITEM', deleteItem)
+}
+
+export default shelfSaga;
